c2e32add10e754579dd5b101b5c315ee98e8a430
ABNmmd <moha.ww.ha@gmail.com>
2026-01-06 23:15:01 +0100

    Implement patient management API with CRUD operations

--- patient-service-api/src/main/java/ma/fsr/soa/patientserviceapi/web/PatientController.java
+++ patient-service-api/src/main/java/ma/fsr/soa/patientserviceapi/web/PatientController.java
@@ -1,11 +1,42 @@
 package ma.fsr.soa.patientserviceapi.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ma.fsr.soa.cabinetrepo.model.Patient;
+import ma.fsr.soa.patientserviceapi.dto.PatientUpdateDto;
+import ma.fsr.soa.patientserviceapi.service.PatientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/internal/api/v1/patients")
 @AllArgsConstructor
 public class PatientController {
+
+    private final PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/{id}")
+    public Patient getPatientById(@PathVariable Long id) throws Exception {
+        return patientService.getPatientById(id);
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient) throws Exception {
+        return patientService.createPatient(patient);
+    }
+
+    @PutMapping("/{id}")
+    public Patient updatePatient(@PathVariable Long id, @RequestBody PatientUpdateDto patientDto) throws Exception {
+        return patientService.updatePatient(id, patientDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+    }
 }
