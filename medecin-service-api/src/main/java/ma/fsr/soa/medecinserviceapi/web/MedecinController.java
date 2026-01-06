40073436eea146b01801e87656bc9f7864ad7b87
ABNmmd <moha.ww.ha@gmail.com>
2026-01-07 00:04:13 +0100

    Implement Medecin management with CRUD operations

--- medecin-service-api/src/main/java/ma/fsr/soa/medecinserviceapi/web/MedecinController.java
+++ medecin-service-api/src/main/java/ma/fsr/soa/medecinserviceapi/web/MedecinController.java
@@ -1,11 +1,42 @@
 package ma.fsr.soa.medecinserviceapi.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ma.fsr.soa.cabinetrepo.model.Medecin;
+import ma.fsr.soa.medecinserviceapi.dto.MedecinUpdateDto;
+import ma.fsr.soa.medecinserviceapi.service.MedecinService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/internal/api/v1/medecins")
 @AllArgsConstructor
 public class MedecinController {
+
+    private final MedecinService medecinService;
+
+    @GetMapping
+    public List<Medecin> getAllMedecins() {
+        return medecinService.getAllMedecins();
+    }
+
+    @GetMapping("/{id}")
+    public Medecin getMedecinById(@PathVariable Long id) throws Exception {
+        return medecinService.getMedecinById(id);
+    }
+
+    @PostMapping
+    public Medecin createMedecin(@RequestBody Medecin medecin) throws Exception {
+        return medecinService.createMedecin(medecin);
+    }
+
+    @PutMapping("/{id}")
+    public Medecin updateMedecin(@PathVariable Long id, @RequestBody MedecinUpdateDto medecinDto) throws Exception {
+        return medecinService.updateMedecin(id, medecinDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMedecin(@PathVariable Long id) {
+        medecinService.deleteMedecin(id);
+    }
 }
